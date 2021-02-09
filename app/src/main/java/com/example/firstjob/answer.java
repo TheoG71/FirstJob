2ce0913746241518d14c48e7063179be6af297f6
me <me@example.com>
2021-02-09 13:43:33 +0100

    commit answer1

--- app/src/main/java/com/example/firstjob/answer.java
+++ app/src/main/java/com/example/firstjob/answer.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class answer extends AppCompatActivity {
-
+// Voici un commentaire pour pouvoir push
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
